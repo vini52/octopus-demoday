81e9d9b1553a77a3682e7f85d812882340c6a0c1
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2019-07-10 17:52:10 -0300

    adicionar pilares

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,7 +1,7 @@
 
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 50) {
       document.getElementById("octopusLogo").src = "/static/icones/logo2.png";
       navbar.classList.add('fixedMenu');
     } else {
