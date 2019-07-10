1e644d414c346da6447301d8b2d23d29f0c52c26
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2019-07-10 16:09:35 -0300

    corrigir navbar

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,13 +1,13 @@
 
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-      document.getElementById("octopusLogo").src = "/static/icones/logo2.png";
-      navbar.classList.add('fixedMenu');
-    } else {
-      document.getElementById("octopusLogo").src = "/static/icones/polvinho1.png";
-      navbar.classList.remove('fixedMenu');
-    }
-  }
+// window.onscroll = function() {scrollFunction()};
+// function scrollFunction() {
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//       document.getElementById("octopusLogo").src = "/static/icones/logo2.png";
+//       navbar.classList.add('fixedMenu');
+//     } else {
+//       document.getElementById("octopusLogo").src = "/static/icones/polvinho1.png";
+//       navbar.classList.remove('fixedMenu');
+//     }
+//   }
   
   
