a648819945956803b90e4edcb8231a8d06cb630b
Vinicius Araujo <varaujo.silva03@gmail.com>
2019-07-10 23:26:24 -0300

    Fazer section Pilares

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,7 +1,7 @@
 
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 50) {
+    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 2.5) {
       document.getElementById("octopusLogo").src = "/static/icones/logo2.png";
       navbar.classList.add('fixedMenu');
     } else {
